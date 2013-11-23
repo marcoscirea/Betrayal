7813e27cf50962d8903bd0dff4b1e3c230091ff7
marcoscirea <marco.prolog@gmail.com>
2013-11-23 15:18:13 +0100

    Card textures added + card values

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -6,18 +6,23 @@ public class Item : MonoBehaviour {
 	public string player;
 	public string type;
 	public int value;
+	public int uses;
+	public Material[] food = new Material[5];
+	int[] food_data = {1,1,1,1,1};
+	public Material[] equipment = new Material[2];
+	int[,] equipment_data = { {2,2}, {3,2} };
 
 	// Use this for initialization
 	void Start () {
 		if (Random.value <=0.7){
 			//card is food
 			type= "food";
-			value = 1;
+			randomFood();
 		}
 		else {
 			//card is equipment
 			type= "eqip";
-			value = 1;
+			randomEquipment();
 		}
 	}
 	
@@ -30,5 +35,35 @@ public class Item : MonoBehaviour {
 		Player p = GameObject.Find(player).GetComponent<Player>();
 		if (p.isActive)
 			p.useItem(gameObject);
+	}
+
+	void randomFood(){
+		float r = Random.value;
+		//	Debug.Log("r "+r); 
+		float percentage = 1f/food.Length;
+		int i = 0;
+		while (r > percentage*(i+1)){
+			//Debug.Log(percentage*(i+1));
+			i++;
+		}
+		renderer.material=food[i];
+
+		value=food_data[i];
+		uses=1;
+	}
+
+	void randomEquipment() {
+		float r = Random.value;
+		//	Debug.Log("r "+r); 
+		float percentage = 1f/equipment.Length;
+		int i = 0;
+		while (r > percentage*(i+1)){
+			//Debug.Log(percentage*(i+1));
+			i++;
+		}
+		renderer.material=equipment[i];
+		
+		value = equipment_data[i,0];
+		uses=equipment_data[i,1];
 	}
 }
