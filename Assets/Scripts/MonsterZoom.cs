40c09395a6a1260e50225835b3de3a44ef03f8c7
marcoscirea <marco.prolog@gmail.com>
2013-11-25 15:31:19 +0100

    Added zoom for monster

---
+++ Assets/Scripts/MonsterZoom.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterZoom : MonoBehaviour {
+
+	Vector3 original_position;
+	Vector3 original_scale;
+	bool scale=true;
+	// Use this for initialization
+	void Start () {
+		original_scale=transform.localScale;
+		original_position=transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scaleDown();
+	}
+
+	void OnMouseEnter() {
+			transform.position += new Vector3(0,0,-3);
+			scale=false;
+			//transform.localScale += new Vector3(1,1,0);
+	}
+	
+	void OnMouseExit() {
+		transform.position=original_position;
+		//transform.localScale = original_scale;
+		scale=true;
+	}
+	
+	void OnMouseOver(){
+			if (transform.position==original_position){
+				transform.position += new Vector3(0,0,-3);
+				scale=false;
+			}
+			if (transform.localScale.x <= original_scale.x + 0.5f)
+				transform.localScale += new Vector3(1,1,0)*Time.deltaTime;
+	}
+	
+	void scaleDown(){
+		if (scale){
+			if (transform.localScale.x > original_scale.x){
+				transform.localScale -= new Vector3(1,1,0)*Time.deltaTime;
+			}
+			else
+				transform.localScale=original_scale;
+		}
+	}
+}
